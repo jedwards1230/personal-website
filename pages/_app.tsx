fca408c792326a8d3cde8200972f87bb7186105e
Justin Edwards <justinedwards1230@gmail.com>
2022-08-13 13:23:46 -0400

    fixed dark mode flash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Component {...pageProps} />
+		<ThemeProvider>
+			<Component {...pageProps} />
+		</ThemeProvider>
 	)
 }
 
