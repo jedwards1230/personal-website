14ea61e98e800081faeedb4dff61ee44768ab811
Justin Edwards <justinedwards1230@gmail.com>
2022-08-28 16:07:55 -0400

    implement game of wasm

--- components/gameoflife/gameComponent.tsx
+++ components/gameoflife/gameComponent.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useTheme } from "next-themes";
 import { useEffect, useRef } from "react";
-import { GameOfLife } from "./game";
 import styles from "./Game.module.css";
+import { Universe } from "../../game/game";
 
 const Game = () => {
     const { resolvedTheme } = useTheme()
@@ -10,34 +10,66 @@ const Game = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const gridRef = useRef<HTMLCanvasElement>(null);
 
-    const game = useRef(new GameOfLife());
+    const CELL_SIZE = 6;
 
-    const cellColors = {
-        dark: {
-            activeColors: ['#4a148c', '#0d47a1'],
-            inactiveColor: '#050505',
-            lineColor: '#fff'
-        },
-        light: {
-            activeColors: ['#64b5f6', '#ba68c8'],
-            inactiveColor: '#fff',
-            lineColor: '#000'
-        }
+    const height = Math.round(window.innerHeight / CELL_SIZE);
+    const width = Math.round(window.innerWidth / CELL_SIZE);
+
+    const universe = useRef(Universe.new(width, height, CELL_SIZE));
+
+    const setStyle = () => {
+        const primaryCell = getComputedStyle(document.body).getPropertyValue('--primary-cell');
+        const secondaryCell = getComputedStyle(document.body).getPropertyValue('--secondary-cell');
+        const inactiveCell = getComputedStyle(document.body).getPropertyValue('--inactive-cell');
+
+        universe.current.set_style(
+            primaryCell,
+            secondaryCell,
+            inactiveCell,
+        )
     }
+
+    const drawGrid = (ctx: CanvasRenderingContext2D) => {
+        ctx.save();
+        ctx.strokeStyle = getComputedStyle(document.body).getPropertyValue('--grid-line');
+        ctx.globalAlpha = 0.1;
+
+        // Vertical lines.
+        for (let i = 0; i <= width; i++) {
+            ctx.beginPath();
+            ctx.moveTo(i * CELL_SIZE, 0);
+            ctx.lineTo(i * CELL_SIZE, CELL_SIZE * height + 1);
+            ctx.stroke();
+        }
+
+        // Horizontal lines.
+        for (let j = 0; j <= height; j++) {
+            ctx.beginPath();
+            ctx.moveTo(0, j * CELL_SIZE);
+            ctx.lineTo(CELL_SIZE * width + 1, j * CELL_SIZE);
+            ctx.stroke();
+        }
+
+        ctx.globalAlpha = 1.0;
+        ctx.restore();
+    };
 
     const animate = (time: number = 0) => {
         animFrame.current = requestAnimationFrame(animate)
         const canvas = canvasRef.current as HTMLCanvasElement;
 
         if (canvas) {
-            const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-            game.current.update(ctx);
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+            universe.current.tick(ctx);
         }
     }
 
     // listen for color mode changes
     useEffect(() => {
-        game.current.style = resolvedTheme === 'dark' ? cellColors.dark : cellColors.light
+        setStyle()
     }, [resolvedTheme]);
 
     // initialize
@@ -46,25 +78,24 @@ const Game = () => {
         const canvas = canvasRef.current as HTMLCanvasElement;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        game.current.setupCanvas(canvas);
-        game.current.style = resolvedTheme === 'dark' ? cellColors.dark : cellColors.light
+        setStyle();
 
         // establish grid overlay canvas
         const grid = gridRef.current as HTMLCanvasElement;
         grid.width = window.innerWidth;
         grid.height = window.innerHeight;
         const ctx = grid.getContext("2d") as CanvasRenderingContext2D;
-        game.current.drawOverlay(ctx);
+        drawGrid(ctx);
 
         if (resolvedTheme !== undefined) animate();
         return () => cancelAnimationFrame(animFrame.current);
     }, []);
 
     return (
-        <>
+        <div className={styles.container}>
             <canvas className={styles.game} ref={canvasRef} />
             <canvas className={styles.grid} ref={gridRef} />
-        </>
+        </div>
     )
 }
 
