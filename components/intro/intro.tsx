1a41f8fc3354fc99ca6befa65e5422b05a0cf6e2
Justin Edwards <justinedwards1230@gmail.com>
2022-08-28 16:07:55 -0400

    implement game of wasm

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,9 +1,6 @@
 import NextLink from "next/link"
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import Image from "next/image";
 import styles from './Intro.module.css';
-
 
 const Intro = (props: {
     reset: () => void
@@ -11,29 +8,44 @@ const Intro = (props: {
     return (
         <>
             <NextLink href="/" passHref>
-                <a 
-                    className={styles.header} 
+                <a
+                    className={styles.header}
                     onClick={props.reset}
-                    >Justin Edwards</a>
+                >Justin Edwards</a>
             </NextLink>
             <div className={styles.icons}>
                 <a
                     href="//www.github.com/jedwards1230"
                     target="_blank"
                     rel="noreferrer">
-                    <GitHubIcon />
+                    <Image
+                        src="/github-brands.svg"
+                        alt='Github Icon'
+                        className={styles.icon}
+                        height={30}
+                        width={30} />
                 </a>
                 <a
                     href="//www.linkedin.com/in/justinedwards1230"
                     target="_blank"
                     rel="noreferrer">
-                    <LinkedInIcon />
+                    <Image
+                        src="/linkedin-brands.svg"
+                        alt='LinkedIn Icon'
+                        className={styles.icon}
+                        height={30}
+                        width={30} />
                 </a>
                 <a
                     href="mailto:justinedwards1230@gmail.com"
                     target="_blank"
                     rel="noreferrer">
-                    <AlternateEmailIcon />
+                    <Image
+                        src="/at-solid.svg"
+                        alt='Email Icon'
+                        className={styles.icon}
+                        height={30}
+                        width={30} />
                 </a>
             </div>
         </>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  webpack(config, { isServer }) {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+    if (isServer) {
+      config.output.webassemblyModuleFilename =
+        './../static/wasm/[modulehash].wasm';
+    } else {
+      config.output.webassemblyModuleFilename =
+        'static/wasm/[modulehash].wasm';
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
