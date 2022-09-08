ea5e2573e23bb82ab420fffeb75381047e03dcda
Justin Edwards <justinedwards1230@gmail.com>
2022-09-07 23:18:38 -0400

    update nossr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,14 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import '../styles/globals.css'
-import NoSsr from '../scripts/noSsr';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	usePageViews();
 
 	return (
 		<ThemeProvider disableTransitionOnChange>
-			<NoSsr>
-				<GoogleAnalytics strategy='lazyOnload' />
-				<Component {...pageProps} />
-			</NoSsr>
+			<GoogleAnalytics strategy='lazyOnload' />
+			<Component {...pageProps} />
 		</ThemeProvider>
 	)
 }
