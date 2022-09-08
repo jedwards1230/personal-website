1b82112ec32421ccca512910e72c51d6b4aa4270
Justin Edwards <justinedwards1230@gmail.com>
2022-09-08 00:01:41 -0400

    added amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,12 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import '../styles/globals.css'
+import Amplify from 'aws-amplify';
+import config from '../aws-exports';
+
+Amplify.configure({
+  ...config, ssr: true
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	usePageViews();
