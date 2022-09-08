dca45cd7c5fdf31dcc13025818837c747e7c29c7
Justin Edwards <justinedwards1230@gmail.com>
2022-09-08 00:33:20 -0400

    added amplify analytics

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "personalwebsite": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "personalwebsite": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import '../styles/globals.css'
-import Amplify from 'aws-amplify';
+import { Amplify, Analytics } from 'aws-amplify';
 import config from '../aws-exports';
 
 Amplify.configure({
   ...config, ssr: true
 });
+
+Analytics.record({ name: 'pageVisit' });
 
 function MyApp({ Component, pageProps }: AppProps) {
 	usePageViews();
