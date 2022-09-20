8350bc04174155b1b42b5e48425eb39b75b59d02
Justin <justinedwards1230@gmail.com>
2022-09-20 14:14:33 -0400

    add SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
 import '../styles/globals.css'
+import { useState, useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	usePageViews();
+	const [isMounted, setIsMounted] = useState(false);
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
+	if (!isMounted) return null;
 
 	return (
 		<ThemeProvider disableTransitionOnChange>
