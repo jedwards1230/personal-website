458e80b7da99180a208050a8260310905fd77cdc
Justin Edwards <justinedwards1230@gmail.com>
2023-04-26 23:20:11 -0400

    fix: boolean

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ const BLOCKED_COUNTRIES = ['RU', 'CN', 'DE', 'IE', 'LU'];
 export function middleware(request: NextRequest) {
     const country = (request.geo && request.geo.country) || 'US';
 
-    if (!BLOCKED_COUNTRIES.includes(country)) {
+    if (BLOCKED_COUNTRIES.includes(country)) {
         return NextResponse.redirect('/404');
     }
 
