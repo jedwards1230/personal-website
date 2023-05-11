9ef3476e3da61a6fbf62d2c148069a758699d4b3
Justin Edwards <justinedwards1230@gmail.com>
2023-05-11 00:25:28 -0400

    feat: basic chat suggestions

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -5,7 +5,7 @@ import { ChatContextProvider } from '@/lib/chatContext';
 export default async function Chat() {
     const initialState = await getInitialState();
     return (
-        <ChatContextProvider initialState={initialState}>
+        <ChatContextProvider initialChat={initialState}>
             <div className="flex w-full max-w-2xl flex-col gap-4 p-4 dark:gap-2 md:w-auto md:max-w-5xl lg:max-w-7xl">
                 <ChatInterface />
             </div>
