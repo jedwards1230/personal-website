9c47691cecff5595880e11f39343ef393e9c44f0
Justin Edwards <justinedwards1230@gmail.com>
2023-06-01 02:26:22 -0400

    feat: switch to langchain

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,14 +1,14 @@
 import { getInitialState } from '@/lib/gpt';
 import ChatInterface from './ChatInterface';
-import { ChatContextProvider } from '@/lib/chatContext';
+import { ChatProvider } from '@/lib/chatContext';
 
 export default async function Chat() {
     const initialState = await getInitialState();
     return (
-        <ChatContextProvider initialChat={initialState}>
+        <ChatProvider initialChat={initialState}>
             <div className="flex w-full max-w-2xl flex-col gap-4 p-4 dark:gap-2 md:w-auto md:max-w-5xl lg:max-w-7xl">
                 <ChatInterface />
             </div>
-        </ChatContextProvider>
+        </ChatProvider>
     );
 }
