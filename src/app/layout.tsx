4e51096ff63853649229ea9c1330020d92355b80
Justin <justinedwards1230@gmail.com>
2023-07-18 10:58:09 -0400

    chore: disable chat temporarily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <html
             suppressHydrationWarning={true}
             lang="en"
-            className="bg-white dark:bg-black"
+            className="h-full w-full bg-white dark:bg-black"
         >
             <body className="container flex h-full w-full flex-col items-center justify-center overflow-y-scroll py-4 text-center sm:py-8 md:py-16">
                 <Providers>{children}</Providers>
