8834849c557cff2b1e243634466bebb4479affdf
Justin <justinedwards1230@gmail.com>
2023-08-17 13:35:47 -0400

    chore: update details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             {/* Details Panel */}
             <div
                 className={clsx(
-                    'absolute flex h-full flex-col overflow-y-scroll border-neutral-400 bg-neutral-100/90 px-2 pb-1 pt-2 transition-all dark:border-neutral-600 dark:bg-neutral-900/90 sm:static sm:bg-neutral-100 sm:pr-12 dark:sm:bg-neutral-900',
+                    'absolute flex h-full flex-col overflow-y-scroll border-neutral-400 bg-neutral-100/90 px-2 pb-1 pt-2 transition-all dark:border-neutral-600 dark:bg-neutral-900/90 sm:static sm:bg-neutral-50 sm:pr-12 dark:sm:bg-neutral-900',
                     open
                         ? 'right-0 w-full sm:w-1/2 sm:border-l lg:w-1/3'
                         : '-right-48 w-0 translate-x-full',
@@ -49,7 +49,7 @@ export default function Home() {
             <button
                 onClick={() => setOpen((open) => !open)}
                 className={clsx(
-                    'absolute right-4 top-4 flex h-12 w-12 items-center justify-center rounded-full bg-blue-500/50 transition-colors hover:bg-blue-500/30 sm:right-0 sm:top-auto sm:h-full sm:rounded-none sm:bg-blue-500/20',
+                    'absolute right-4 top-4 flex h-12 w-12 items-center justify-center rounded-full bg-blue-500/70 transition-colors hover:bg-blue-500/30 sm:right-0 sm:top-auto sm:h-full sm:rounded-none sm:bg-blue-500/20',
                 )}
             >
                 <span
