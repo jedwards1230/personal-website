1f43b6d0945bd8843e800643a761747424914394
Justin <justinedwards1230@gmail.com>
2023-08-17 16:36:39 -0400

    chore: update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import type { Metadata } from 'next/types';
+
 import Home from '@/components/Home';
 
-export const metadata = {
-    title: 'J. Edwards',
-    description: 'Personal website for Justin Edwards',
-};
-
 export const runtime = 'edge';
+
+const APP_NAME = 'J. Edwards Personal Website';
+const APP_DEFAULT_TITLE = 'J. Edwards';
+const APP_TITLE_TEMPLATE = 'J. Edwards | %s';
+const APP_DESCRIPTION = 'Personal website for Justin Edwards';
+
+export const metadata: Metadata = {
+    applicationName: APP_NAME,
+    title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+};
 
 export default async function Page() {
     return <Home />;
