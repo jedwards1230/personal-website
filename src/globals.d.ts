d41895a17f121a009101a123bc17077352b478cb
Justin Edwards <justinedwards1230@gmail.com>
2023-08-25 04:08:22 -0400

    feat: update db

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,9 @@
 type Section = 'about' | 'experience' | 'projects' | 'contact';
+
+type Experience = {
+    title: string;
+    company: string;
+    period: string;
+    description: string[];
+    tags: string[];
+};
