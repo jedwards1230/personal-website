ea00ad52c24b5b10c99ac6d5388d5e04cda73452
Justin Edwards <justinedwards1230@gmail.com>
2023-08-25 12:44:15 -0400

    feat: more tags

--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,44 +1,19 @@
 import Link from 'next/link';
 import Image from 'next/image';
+
 import Section from '@/components/Section';
+import TagList from '@/components/Tag';
+import { projects } from './data';
 
 export default function Projects() {
-    const projects = [
-        {
-            title: 'ChatUI',
-            description: 'UI for managing chats with LLMs',
-            href: 'https://github.com/jedwards1230/chat',
-            preview:
-                'https://raw.githubusercontent.com/jedwards1230/chat/main/screenshots/screenshot-0.png',
-        },
-        {
-            title: 'SearchAI',
-            description: 'AI assisted search engine',
-            href: 'https://github.com/jedwards1230/search',
-            preview:
-                'https://raw.githubusercontent.com/jedwards1230/search/main/home.png',
-        },
-        {
-            title: 'go-kerbal',
-            description: 'CLI for managing Kerbal Space Program mods',
-            href: 'https://github.com/jedwards1230/go-kerbal',
-            preview:
-                'https://raw.githubusercontent.com/jedwards1230/go-kerbal/main/extras/screenshots/main.png',
-        },
-        {
-            title: 'Personal Website',
-            description: 'My corner of the internet',
-            href: 'https://github.com/jedwards1230/personal-website',
-            preview:
-                'https://raw.githubusercontent.com/jedwards1230/personal-website/main/screenshot.png',
-        },
-    ];
     return (
         <Section id="projects">
             <div className="group/list flex flex-col gap-4">
                 {projects.map((p, i) => {
                     return (
-                        <div
+                        <Link
+                            href={p.href}
+                            target="_blank"
                             className="group grid w-full grid-cols-12 gap-1 rounded p-2 text-neutral-500 transition-all dark:text-neutral-400 lg:gap-4 hover:lg:bg-neutral-100 hover:lg:!opacity-100 group-hover/list:lg:opacity-50 hover:lg:dark:bg-neutral-800"
                             key={'projects-' + i}
                         >
@@ -51,18 +26,15 @@ export default function Projects() {
                                 className="col-span-12 aspect-video w-full select-none bg-neutral-100 lg:col-span-4"
                             />
                             {/* Title */}
-                            <Link
-                                href={p.href}
-                                target="_blank"
-                                className="col-span-12 flex flex-col gap-1 lg:col-span-8"
-                            >
+                            <div className="col-span-12 flex flex-col gap-1 lg:col-span-8">
                                 <div className="font-medium text-neutral-950 dark:text-neutral-50">
                                     {p.title}
                                 </div>
                                 {/* Description */}
                                 <div>{p.description}</div>
-                            </Link>
-                        </div>
+                                <TagList tags={p.tags} />
+                            </div>
+                        </Link>
                     );
                 })}
             </div>
---
+++ src/app/data.ts
@@ -0,0 +1,79 @@
+export const projects: Project[] = [
+    {
+        title: 'ChatUI',
+        description: 'UI for managing chats with LLMs',
+        href: 'https://github.com/jedwards1230/chat',
+        tags: ['TypeScript', 'NextJS', 'TailwindCSS', 'OpenAI API', 'Supabase'],
+        preview:
+            'https://raw.githubusercontent.com/jedwards1230/chat/main/screenshots/screenshot-0.png',
+    },
+    {
+        title: 'SearchAI',
+        description: 'AI assisted search engine',
+        href: 'https://github.com/jedwards1230/search',
+        tags: ['TypeScript', 'NextJS', 'TailwindCSS', 'OpenAI API', 'Supabase'],
+        preview:
+            'https://raw.githubusercontent.com/jedwards1230/search/main/home.png',
+    },
+    {
+        title: 'go-kerbal',
+        description: 'CLI for managing Kerbal Space Program mods',
+        href: 'https://github.com/jedwards1230/go-kerbal',
+        tags: ['Go', 'CLI'],
+        preview:
+            'https://raw.githubusercontent.com/jedwards1230/go-kerbal/main/extras/screenshots/main.png',
+    },
+    {
+        title: 'Personal Website',
+        description: 'My corner of the internet',
+        href: 'https://github.com/jedwards1230/personal-website',
+        tags: ['TypeScript', 'NextJS', 'TailwindCSS'],
+        preview:
+            'https://raw.githubusercontent.com/jedwards1230/personal-website/main/screenshot.png',
+    },
+];
+
+export const experience: Experience[] = [
+    {
+        title: 'Full Stack Developer',
+        company: 'Atlantis Health',
+        period: '08/2021 - Present',
+        description: [
+            'Delivered full-stack applications for multiple clients using agile methodologies and led the development of a large-scale NextJs website.',
+            "Implemented IBM Watson's NLP capabilities in a patient treatment support application and improved legacy app performance significantly.",
+            'Enhanced CI/CD deploy times on AWS infrastructure and fostered effective teamwork leading to projects completion ahead of schedule.',
+        ],
+        tags: [
+            'TypeScript',
+            'NextJs',
+            'Python',
+            'SQL',
+            'AWS',
+            'TailwindCSS',
+            'IBM Watson',
+            'Asana',
+        ],
+    },
+    {
+        title: 'Product Support Specialist',
+        company: 'VantagePoint AI',
+        period: '01/2021 - 11/2021',
+        description: [
+            'Maintained knowledge of AI software platform and identified bugs, enhancing product stability.',
+            'Resolved support tickets with exceptional customer service and coordinated with Software Potential for customer info management.',
+            'Assisted users in troubleshooting software issues and created helpful knowledge base articles.',
+        ],
+        tags: ['Bug Reports', 'Salesforce', 'Software Potential'],
+    },
+    {
+        title: 'Entertainment Technician',
+        company: 'Freelance',
+        period: '03/2015 - 05/2022',
+        description: [
+            'Led and trained crews for events and resolved technical issues.',
+            'Steered problem-solving initiatives during productions to ensure smooth events.',
+            'Participated in various aspects of production.',
+        ],
+        tags: ['Leadership', 'Project Planning', 'Troubleshooting'],
+    },
+];
