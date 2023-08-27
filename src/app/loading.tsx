4607166ec40fe33dd9084c3fcdbfe8179a342298
Justin Edwards <justinedwards1230@gmail.com>
2023-08-27 01:12:29 -0400

    fix: remove log

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,3 @@
 export default function Loading() {
-    return (
-        <p className="flex h-full w-full items-center justify-center">
-            Loading......
-        </p>
-    );
+    return <p />;
 }
