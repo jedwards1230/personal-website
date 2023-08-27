771a22b12b1e7f51a6dd5da6c2550da798789ac8
Justin Edwards <justinedwards1230@gmail.com>
2023-08-27 18:08:01 -0400

    feat: back arrow

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -10,13 +10,26 @@ type Experience = {
 
 type Project = {
     id: string;
+    /** Year of completion */
     year: number;
+    /** Month of completion */
     month: number;
+    /** Who the project was built for */
     client: string;
+    /** Title of the project */
     title: string;
+    /** Short description of the project */
     description: string;
+    /** Additional info about the project */
+    info?: string;
+    /** Link to the project */
     href?: string;
+    /** Tags for the project */
     tags: string[];
+    /** Whether to showcase the project on the home page */
     showcase?: boolean;
+    /** Image to display for the project */
     img?: string;
+    /** List of images to display for the project */
+    imgList?: string[];
 };
