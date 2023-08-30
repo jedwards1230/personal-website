560c40aa4a26386036ef776c5b6b9a9d174be363
Justin <justinedwards1230@gmail.com>
2023-08-30 16:51:09 -0400

    feat: simple resume route

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -8,7 +8,7 @@ export default function DownloadResume() {
     const plausible = usePlausible();
     return (
         <Link
-            href="/Justin Edwards - Resume.pdf"
+            href="/resume"
             onClick={() => plausible('Resume Download')}
             target="_blank"
             className="inline-flex select-none justify-center gap-2 pt-8 text-lg hover:underline"
