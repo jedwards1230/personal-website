34a0c4855ce7181b29cec82044de5252a10be10b
Justin <justinedwards1230@gmail.com>
2023-08-30 16:21:10 -0400

    fix: spacing

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -68,7 +68,7 @@ export default function ProjectCard({
                 </div>
             </div>
             {project.info && (
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
                     <Markdown>{project.info}</Markdown>
                 </div>
             )}
