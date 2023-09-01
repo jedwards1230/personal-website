d3414357d4e66100a154a751a1bcb224dcbd7e8b
Justin Edwards <justinedwards1230@gmail.com>
2023-09-01 13:02:28 -0400

    refactor: title data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 import { experiences } from './experience';
 import { projects } from './projects';
+import { title } from './title';
 
-export { experiences, projects };
+export { experiences, projects, title };
