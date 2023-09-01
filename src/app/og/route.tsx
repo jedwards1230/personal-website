4fbe3497a9e6184046245aadd0e70d4159b79465
Justin <justinedwards1230@gmail.com>
2023-08-31 23:51:15 -0400

    fix: og image

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -5,7 +5,7 @@ export const runtime = 'edge';
 export async function GET() {
     return new ImageResponse(
         (
-            <div tw="flex flex-col w-full bg-white h-full justify-center pb-16 pl-8">
+            <div tw="flex flex-col w-full bg-white h-full justify-center pb-16 pl-12">
                 <div tw="text-7xl font-semibold pb-2">Justin Edwards</div>
                 <div tw="text-4xl font-semibold tracking-wide text-neutral-500 pl-2">
                     Full Stack Developer
@@ -13,7 +13,7 @@ export async function GET() {
             </div>
         ),
         {
-            width: 1200,
+            width: 1000,
             height: 600,
         },
     );
