bc2237e0fd4169bf0fd448d9ba009db52f457480
Justin <justinedwards1230@gmail.com>
2023-09-01 19:13:18 -0400

    feat: new image

--- src/components/cards/ImageCard.tsx
+++ src/components/cards/ImageCard.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ChevronLeft, ChevronRight } from '../Icons';
+import { usePlausible } from 'next-plausible';
 
 export default function ImageCard({ project }: { project: Project }) {
+    const plausible = usePlausible();
     const [idx, setIdx] = useState(0);
 
     // go to next image
@@ -25,13 +27,23 @@ export default function ImageCard({ project }: { project: Project }) {
         }
     };
 
+    // track image per project
+    useEffect(() => {
+        plausible('Image', {
+            props: {
+                project: project.title,
+                image: project.images[idx],
+            },
+        });
+    }, [idx, plausible, project.images, project.title]);
+
     return (
         <div className="relative h-full w-full">
             {project.images[idx - 1] && (
                 <div className="absolute inset-y-0 left-0 flex items-center">
                     <button
                         onClick={prev}
-                        className="flex h-12 w-12 items-center justify-center rounded-r bg-background/20 transition-all hover:bg-background/80"
+                        className="flex h-12 w-12 items-center justify-center rounded-r border border-border bg-background/20 transition-all hover:bg-background/80"
                     >
                         <ChevronLeft />
                     </button>
@@ -41,7 +53,7 @@ export default function ImageCard({ project }: { project: Project }) {
                 <div className="absolute inset-y-0 right-0 flex items-center">
                     <button
                         onClick={next}
-                        className="flex h-12 w-12 items-center justify-center rounded-l bg-background/20 transition-all hover:bg-background/80"
+                        className="flex h-12 w-12 items-center justify-center rounded-l border border-border transition-all hover:bg-background/80 dark:bg-background/20"
                     >
                         <ChevronRight />
                     </button>
@@ -50,6 +62,7 @@ export default function ImageCard({ project }: { project: Project }) {
             <Image
                 width={1920}
                 height={1080}
+                key={project.images[idx]}
                 src={project.images[idx]}
                 alt={project.title}
                 className="aspect-video select-none"
