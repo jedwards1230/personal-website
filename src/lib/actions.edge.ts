2b437773660aed66e652628a76c0760f4520e960
Justin Edwards <justinedwards1230@gmail.com>
2023-09-05 02:11:07 -0400

    fix: prisma config

--- src/lib/actions.edge.ts
+++ src/lib/actions.edge.ts
@@ -1,7 +1,12 @@
 import { prisma } from './prisma.edge';
 
+const config = {
+    // cache for 24 hours
+    cacheStrategy: { ttl: 60 * 60 * 24 },
+};
+
 export async function getAllExperiences(): Promise<Experience[]> {
-    const experiences = await prisma.experience.findMany();
+    const experiences = await prisma.experience.findMany(config);
     return experiences.map((experience) => ({
         ...experience,
         extraTags: experience.extraTags ? experience.extraTags.split(',') : [],
@@ -9,6 +14,6 @@ export async function getAllExperiences(): Promise<Experience[]> {
 }
 
 export async function getAllProjects(): Promise<Project[]> {
-    const projects = await prisma.project.findMany();
+    const projects = await prisma.project.findMany(config);
     return projects;
 }
