ee11d7dd62a78d54fba29edff5648113d2be1f67
Justin Edwards <justinedwards1230@gmail.com>
2023-09-05 01:57:53 -0400

    feat(admin): ui view updates

--- src/components/dialogs/admin/ProjectDialog.tsx
+++ src/components/dialogs/admin/ProjectDialog.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { useState } from 'react';
-
-import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { GenericDialog } from './GenericDialog';
 import ProjectForm from '@/components/forms/admin/ProjectForm';
+import ProjectView from './ProjectView';
 
 export function ProjectDialog({
     children,
@@ -12,12 +11,14 @@ export function ProjectDialog({
     children: React.ReactNode;
     project?: Project;
 }) {
-    const [open, setOpen] = useState(false);
-
     return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>{children}</DialogTrigger>
-            <ProjectForm project={project} setOpen={setOpen} />
-        </Dialog>
+        <GenericDialog
+            dataType="project"
+            FormComponent={ProjectForm}
+            ViewComponent={ProjectView}
+            data={project}
+        >
+            {children}
+        </GenericDialog>
     );
 }
