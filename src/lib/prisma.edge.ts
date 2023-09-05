b48c3fe6d91d44dc2488ea7de02535c70b45a736
Justin Edwards <justinedwards1230@gmail.com>
2023-09-05 02:11:07 -0400

    fix: prisma config

--- src/lib/prisma.edge.ts
+++ src/lib/prisma.edge.ts
@@ -1,2 +1,4 @@
 import { PrismaClient } from '@prisma/client/edge';
-export const prisma = new PrismaClient();
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+export const prisma = new PrismaClient().$extends(withAccelerate());
