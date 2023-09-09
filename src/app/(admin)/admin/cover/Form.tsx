744b39292516e904db4c96b9b983006c259a930d
Justin Edwards <justinedwards1230@gmail.com>
2023-09-08 23:08:30 -0400

    fix: cors issue

--- src/app/(admin)/admin/cover/Form.tsx
+++ src/app/(admin)/admin/cover/Form.tsx
@@ -79,7 +79,7 @@ export default function CoverForm({
             },
         ];
         try {
-            const res = await fetch('http://localhost:3000/api/chat', {
+            const res = await fetch('/api/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
