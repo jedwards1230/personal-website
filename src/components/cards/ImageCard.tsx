f02f10f13cb09ac83cca82ba2897793358ac394a
Justin Edwards <justinedwards1230@gmail.com>
2023-09-16 15:16:45 -0400

    feat: image sliding

--- src/components/cards/ImageCard.tsx
+++ src/components/cards/ImageCard.tsx
@@ -2,32 +2,17 @@
 
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { ChevronLeft, ChevronRight } from '../Icons';
 import { usePlausible } from 'next-plausible';
+
+import { ChevronLeft, ChevronRight } from '../Icons';
 
 export default function ImageCard({ project }: { project: Project }) {
     const plausible = usePlausible();
     const [idx, setIdx] = useState(0);
 
-    // go to next image
-    const next = () => {
-        if (idx < project.images.length - 1) {
-            setIdx(idx + 1);
-        } else {
-            setIdx(0);
-        }
-    };
+    const next = () => setIdx(idx < project.images.length - 1 ? idx + 1 : 0);
+    const prev = () => setIdx(idx > 0 ? idx - 1 : project.images.length - 1);
 
-    // go to previous image
-    const prev = () => {
-        if (idx > 0) {
-            setIdx(idx - 1);
-        } else {
-            setIdx(project.images.length - 1);
-        }
-    };
-
-    // track image per project
     useEffect(() => {
         plausible('Image', {
             props: {
@@ -38,35 +23,39 @@ export default function ImageCard({ project }: { project: Project }) {
     }, [idx, plausible, project.images, project.title]);
 
     return (
-        <div className="relative h-full w-full">
-            {project.images[idx - 1] && (
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                    <button
-                        onClick={prev}
-                        className="flex h-12 w-12 items-center justify-center rounded-r border border-border bg-background/20 transition-all hover:bg-background/80"
-                    >
-                        <ChevronLeft />
-                    </button>
-                </div>
-            )}
-            {project.images[idx + 1] && (
-                <div className="absolute inset-y-0 right-0 flex items-center">
-                    <button
-                        onClick={next}
-                        className="flex h-12 w-12 items-center justify-center rounded-l border border-border transition-all hover:bg-background/80 dark:bg-background/20"
-                    >
-                        <ChevronRight />
-                    </button>
-                </div>
-            )}
-            <Image
-                width={1920}
-                height={1080}
-                key={project.images[idx]}
-                src={project.images[idx]}
-                alt={project.title}
-                className="aspect-video select-none"
-            />
+        <div className="relative h-full min-h-[700px] w-full select-none overflow-hidden">
+            <div className="absolute flex h-full w-full">
+                {project.images.map((image, i) => (
+                    <Image
+                        style={{
+                            transform: `translateX(-${idx * 100}%)`,
+                            left: `${i * 100}%`,
+                        }}
+                        key={i}
+                        src={image}
+                        alt={project.title}
+                        width={1920}
+                        height={1080}
+                        className="absolute h-full w-full transition-transform duration-500 ease-in-out"
+                    />
+                ))}
+            </div>
+            <div className="absolute inset-y-0 left-0 flex items-center">
+                <button
+                    onClick={prev}
+                    className="flex h-12 w-12 items-center justify-center rounded-r border border-border bg-background/20 transition-all hover:bg-background/80"
+                >
+                    <ChevronLeft />
+                </button>
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center">
+                <button
+                    onClick={next}
+                    className="flex h-12 w-12 items-center justify-center rounded-l border border-border transition-all hover:bg-background/80 dark:bg-background/20"
+                >
+                    <ChevronRight />
+                </button>
+            </div>
         </div>
     );
 }
