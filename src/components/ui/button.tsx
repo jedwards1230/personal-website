73d2c8a716f341db82ebbac2c233b101a0d15c97
Justin Edwards <justinedwards1230@gmail.com>
2023-09-27 19:05:02 -0400

    chore: bump dependencies

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -45,7 +45,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ({ className, variant, size, asChild = false, ...props }, ref) => {
-        const Comp = asChild ? Slot : 'button';
+        const Comp = asChild ? (Slot as any) : 'button';
         return (
             <Comp
                 className={buttonVariants({ variant, size, className })}
