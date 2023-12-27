86c2d1a0ee497990c61b010b9533a05bf3f48323
Justin <justinedwards1230@gmail.com>
2023-12-26 20:18:24 -0500

    feat: deploy to fly.io

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -30,13 +30,13 @@ export default function handleRequest(
 				responseStatusCode,
 				responseHeaders,
 				remixContext
-		  )
+			)
 		: handleBrowserRequest(
 				request,
 				responseStatusCode,
 				responseHeaders,
 				remixContext
-		  );
+			);
 }
 
 function handleBotRequest(
--- app/routes/resume.ts
+++ app/routes/resume.ts
@@ -2,5 +2,5 @@ import { LoaderFunction, redirect } from "@remix-run/node";
 
 export const loader: LoaderFunction = () => {
 	// Redirect to the PDF file
-	return redirect("/Justin Edwards - Resume.pdf");
+	return redirect("/Justin-Edwards-Resume.pdf");
 };
