0a5685c72ef28e19959ffaa3935eec1f58c9b9f3
Justin <justinedwards1230@gmail.com>
2023-12-31 17:33:51 -0500

    fix: contact form

--- src/app/(index)/Contact.tsx
+++ src/app/(index)/Contact.tsx
@@ -1,12 +1,8 @@
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-
 import DownloadResume from "./DownloadResume";
 import SectionTitle from "./SectionTitle";
 import { getAbout } from "@/models/about.server";
 import { getPageViews } from "@/lib/utils";
+import ContactForm from "@/components/Forms/ContactForm";
 
 const ID = "contact";
 
@@ -72,67 +68,5 @@ export default async function Contact() {
 				{pageViews} {pageViews === 1 ? "visit" : "visits"} this week
 			</div>
 		</section>
-	);
-}
-
-/* export const handleFormSubmit = async (request: Request) => {
-	const formData = await request.formData();
-	const name = String(formData.get("name"));
-	const email = String(formData.get("email"));
-	const message = String(formData.get("message"));
-
-	if (!message) {
-		return json({ error: "Message is required." }, { status: 400 });
-	}
-
-	try {
-		await createContact(name ?? "", email ?? "", message);
-		return json({ success: "Message sent successfully!" });
-	} catch (error: any) {
-		return json({ error: "Failed to send message." }, { status: 400 });
-	}
-}; */
-
-function ContactForm() {
-	const fetcher = {
-		state: "idle",
-		data: {},
-	};
-	const isSending = fetcher.state !== "idle";
-	const submission = fetcher.data as { success?: string; error?: string };
-
-	return (
-		<form
-			method="post"
-			className="flex h-full w-full flex-1 flex-col justify-between"
-		>
-			<div className="flex w-full select-none flex-col gap-2">
-				<p className="mb-2 text-lg">Leave a message</p>
-
-				<div className="flex flex-col justify-between gap-2 md:flex-row md:items-center">
-					<Label>Name</Label>
-					<Input name="name" className="md:w-2/3" />
-				</div>
-				<div className="flex flex-col justify-between gap-2 md:flex-row md:items-center">
-					<Label>Email</Label>
-					<Input className="md:w-2/3" type="email" name="email" />
-				</div>
-				<div className="flex flex-col justify-between gap-2 md:flex-row md:items-center">
-					<Label>Message</Label>
-					<Textarea name="message" className="md:w-2/3" rows={5} />
-				</div>
-				<Button disabled={isSending} variant="ghost" type="submit">
-					Send
-				</Button>
-				<div className="pt-2 text-center">
-					{submission?.success && (
-						<p className="text-green-500">{submission.success}</p>
-					)}
-					{submission?.error && (
-						<p className="text-red-500">{submission.error}</p>
-					)}
-				</div>
-			</div>
-		</form>
 	);
 }
