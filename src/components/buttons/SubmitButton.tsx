af22afadce4576d05fa6c01368bae478952955ec
Justin <justinedwards1230@gmail.com>
2024-01-01 02:28:02 -0500

    fix: contact form

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -4,12 +4,16 @@ import { useFormStatus } from "react-dom";
 
 import { Button } from "../ui/button";
 
-export default function Submit() {
+export default function Submit({
+	children = "Save",
+}: {
+	children?: React.ReactNode;
+}) {
 	const status = useFormStatus();
 
 	return (
 		<Button disabled={status.pending} type="submit">
-			Save
+			{children}
 		</Button>
 	);
 }
