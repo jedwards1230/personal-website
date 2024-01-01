3c7370c0d3e77a40a9e38d1c7eeb0357e19afa43
Justin <justinedwards1230@gmail.com>
2024-01-01 05:46:29 -0500

    chore: hide cv

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -10,12 +10,12 @@ import Link from "next/link";
 export default async function Page() {
 	return (
 		<>
-			<div className="fixed bottom-0 py-8 right-2 md:right-6 lg:right-10 z-10 flex flex-col h-full items-center justify-between gap-4">
+			{/* <div className="fixed bottom-0 py-8 right-2 md:right-6 lg:right-10 z-10 flex flex-col h-full items-center justify-between gap-4">
 				<Button asChild variant="link">
 					<Link href="/cv">cv</Link>
 				</Button>
 				<AdminButton />
-			</div>
+			</div> */}
 			<div className="max-w-screen select-none flex w-full flex-col justify-between gap-4 px-4 pt-8 sm:px-8 md:h-full md:px-16 md:pt-0 lg:px-24 xl:px-32">
 				<Intro />
 				<Suspense fallback={null}>
