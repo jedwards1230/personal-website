d4c3149b816e473f0e3e448c934ecbaa03d3363b
Justin <justinedwards1230@gmail.com>
2024-01-03 22:42:46 -0500

    fix: width

--- src/components/Views/EducationView.tsx
+++ src/components/Views/EducationView.tsx
@@ -3,7 +3,7 @@ export default function EducationView({ data }: { data: Education }) {
 
 	return (
 		<div className="rounded-rounded flex flex-col gap-2 border border-border p-4 transition-all">
-			<div className="w-1/2 pb-4">
+			<div className="pb-4">
 				<p className="text-xl font-bold">{data.school}</p>
 				<p className="text-lg text-secondary-foreground">
 					{data.degree}
