fe7b38490cd6190890eb84f99c972c67887ee057
Justin <justinedwards1230@gmail.com>
2024-01-14 19:25:11 -0500

    fix: mobile grid

--- src/app/(index)/cv/Projects.tsx
+++ src/app/(index)/cv/Projects.tsx
@@ -8,7 +8,7 @@ export default async function Projects() {
 	return (
 		<div className="space-y-2 print:hidden">
 			<div className="text-xl font-semibold">Projects</div>
-			<div className="grid grid-cols-3 gap-4">
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
 				{projects.map(project => (
 					<ProjectDialog
 						project={project}
