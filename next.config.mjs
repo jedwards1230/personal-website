49c2022e9b064f29a4693a507da6940e6fb0eb52
Justin <justinedwards1230@gmail.com>
2024-05-30 11:10:15 -0400

    fix: esm issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-const { PHASE_PRODUCTION_BUILD } = require("next/constants");
-const { withPlausibleProxy } = require("next-plausible");
+import { PHASE_PRODUCTION_BUILD } from "next/constants.js";
+import { withPlausibleProxy } from "next-plausible";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const isProd = process.env.NODE_ENV === PHASE_PRODUCTION_BUILD;
+
+if (!isProd) {
+	await setupDevPlatform();
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -22,4 +27,4 @@ const nextConfig = {
 	},
 };
 
-module.exports = withPlausibleProxy()(nextConfig);
+export default withPlausibleProxy()(nextConfig);
