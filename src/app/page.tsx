3c471fb50292ba9fbc7c466c3756f9434fc31804
Justin <justinedwards1230@gmail.com>
2024-08-02 04:08:38 -0400

    feat: edge runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
 			<p className="text-5xl">Justin Edwards</p>
 			<p className="text-2xl font-semibold">Site Reliability Engineer</p>
-			<p className="text-neutral-400 hover:text-blue-400 transition-colors">
+			<p className="text-neutral-400 transition-colors hover:text-blue-400">
 				<a href="mailto:justin@jedwards.cc">justin@jedwards.cc</a>
 			</p>
 		</main>
