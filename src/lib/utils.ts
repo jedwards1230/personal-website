2421d1e4b7a1be0557e30215f6534de9e2c73a9a
Justin <justinedwards1230@gmail.com>
2024-08-02 04:04:05 -0400

    chore: remove old code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
-
 const isProduction = process.env.NODE_ENV === "production";
 
 export function invariant(
