9902110607f81d7466a20f2d959130efa27d1cf4
Justin <justinedwards1230@gmail.com>
2024-08-10 22:39:47 -0400

    fix: mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<p className="text-5xl">Justin Edwards</p>
-			<p className="text-2xl font-semibold">Site Reliability Engineer</p>
+		<main className="flex min-h-screen flex-col items-center justify-center p-4 text-center sm:p-12 md:p-24">
+			<p className="text-4xl transition-all md:text-5xl">
+				Justin Edwards
+			</p>
+			<p className="text-xl font-semibold transition-all md:text-2xl">
+				Site Reliability Engineer
+			</p>
 			<p className="text-neutral-400 transition-colors hover:text-blue-400">
 				<a href="mailto:justin@jedwards.cc">justin@jedwards.cc</a>
 			</p>
